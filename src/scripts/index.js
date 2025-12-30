bbb7663b7073e596d0ac113dc4b71ae06319c2be
DiligentViking <acore-n@proton.me>
2025-12-30 14:57:48 -0600

    Add the rest of the linked-list functions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,7 +6,7 @@ function Node(value=null, nextNode=null) {
 }
 
 function LinkedList() {
-  const head = Node();
+  const head = Node();  // Note that the head is technically a Node, but it is only used as an entry point
   return {
     append(value) {
       let curr = head;
@@ -15,13 +15,79 @@ function LinkedList() {
       }
       curr.nextNode = Node(value);
     },
-    toString(value) {
-      let curr = head.nextNode;
-      while (curr) {
-        process.stdout.write(`( ${curr.value} ) -> `);
+
+    prepend(value) {
+      head.nextNode = Node(value, head.nextNode);
+    },
+
+    size() {
+      let count = 0;
+      let curr = head;
+      while (curr.nextNode !== null) {
+        count++;
         curr = curr.nextNode;
       }
-      console.log('null');
+      return count;
+    },
+
+    head() {
+      return head.nextNode?.value;
+    },
+
+    tail() {
+      let curr = head;
+      while (curr.nextNode !== null) {
+        curr = curr.nextNode;
+      }
+      return curr.value;
+    },
+
+    at(index) {
+      let curr = head;
+      for (let i = 0; i <= index; i++) {
+        curr = curr.nextNode;
+        if (!curr) return undefined;
+      }
+      return curr.value;
+    },
+
+    pop() {
+      if (head.nextNode) {
+        const tmp = head.nextNode.value;
+        head.nextNode = head.nextNode.nextNode;
+        return tmp;
+      }
+    },
+
+    contains(value) {
+      let curr = head.nextNode;
+      while (curr) {
+        if (curr.value === value) return true;
+        curr = curr.nextNode;
+      }
+      return false;
+    },
+
+    findIndex(value) {
+      let count = 0;
+      let curr = head.nextNode;
+      while (curr) {
+        if (curr.value === value) return count;
+        curr = curr.nextNode;
+        count++;
+      }
+      return -1;
+    },
+
+    toString() {
+      let string = '';
+      let curr = head.nextNode;
+      while (curr) {
+        string += `( ${curr.value} ) -> `;
+        curr = curr.nextNode;
+      }
+      string += 'null';
+      return string;
     },
   };
 }
@@ -36,4 +102,10 @@ list.append('a');
 list.append("warrior's");
 list.append('heart');
 
-list.toString();
+console.log(
+  list.findIndex('a')
+);
+
+console.log(
+  list.toString()
+);
